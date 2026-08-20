02fbe406946b85ece0a94a8fa873a4f782eb635c
Chinmoy Das <114159543+chinmoy1809@users.noreply.github.com>
2026-08-20 16:38:06 +0530

    [94] [Time Beats: 100.00%] [Memory Beats: 15.12%] - LeetPush

--- 94-Binary-Tree-Inorder-Traversal.java
+++ 94-Binary-Tree-Inorder-Traversal.java
@@ -14,16 +14,23 @@
  * }
  */
 class Solution {
-    public void inorder(TreeNode root,List<Integer> list){
-        if(root != null){
-            inorder(root.left,list);
-            list.add(root.val);
-            inorder(root.right,list);
-        }
-    }
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        inorder(root,list);
+        Stack<TreeNode> st = new Stack<>();
+        if(root == null ) return list;
+        TreeNode node = root;
+
+        while(true){
+            if(node != null) {
+                st.push(node);
+                node=node.left;
+            }else{
+                if(st.isEmpty()) break;
+                TreeNode newNode = st.pop();
+                list.add(newNode.val);
+                node = newNode.right;
+            }
+        }
         return list;
     }
 }
