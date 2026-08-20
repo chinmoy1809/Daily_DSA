829476d7e394f34445f5beb5d01ca142e5f3097a
Chinmoy Das <114159543+chinmoy1809@users.noreply.github.com>
2026-08-20 16:52:42 +0530

    [145] [Time Beats: 100.00%] [Memory Beats: 52.98%] - LeetPush

--- 145-Binary-Tree-Postorder-Traversal.java
+++ 145-Binary-Tree-Postorder-Traversal.java
@@ -14,16 +14,21 @@
  * }
  */
 class Solution {
-    public void postorder(TreeNode root,List<Integer> list){
-        if(root != null){
-            postorder(root.left,list);
-            postorder(root.right,list);
-            list.add(root.val);
-        }
-    }
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        postorder(root,list);
+        Stack<TreeNode> st1 = new Stack<>();
+        Stack<TreeNode> st2 = new Stack<>();
+        if(root == null) return list;
+        st1.push(root);
+        while(!st1.isEmpty()){
+            TreeNode node = st1.pop();
+            st2.push(node);
+            if(node.left != null) st1.push(node.left);
+            if(node.right != null) st1.push(node.right);
+        }
+        while(!st2.isEmpty()){
+            list.add(st2.pop().val);
+        }
         return list;
     }
 }
